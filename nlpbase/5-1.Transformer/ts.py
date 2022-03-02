016432d5ddf4c175795dcafee2e50bf928c9d132
dante <974668431@qq.com>
2022-03-02 11:13:49 +0800

    first

---
+++ nlpbase/5-1.Transformer/ts.py
@@ -0,0 +1,31 @@
+import math
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as Data
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        '''
+        x: [seq_len, batch_size, d_model]
+        '''
+        x = x + self.pe[:x.size(0), :]
+        print("---")
+        return self.dropout(x)
+pos_emb = PositionalEncoding(512)
+a = torch.rand(3,2,512)
+b=pos_emb(a)
+print(a.shape)
+print(b)
