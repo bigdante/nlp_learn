16b623a4480c4d305d3c37d37d91a9f51506e075
dante <974668431@qq.com>
2022-04-12 14:14:22 +0800

    first

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def print_hi(name):
 # for test
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    a = 1
+    assert a
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
